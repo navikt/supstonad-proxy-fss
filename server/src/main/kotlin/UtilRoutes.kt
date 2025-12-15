f7b9ef6c4091921b86c9bf34bac79b38bcb21a74
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-12-15 13:24:14 +0100

    Cleanup

--- server/src/main/kotlin/UtilRoutes.kt
+++ server/src/main/kotlin/UtilRoutes.kt
@@ -1,10 +1,8 @@
 package no.nav.supstonad
 
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.log
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.application
 import io.ktor.server.routing.get
 
 fun Route.utilApi() {
@@ -12,8 +10,6 @@ fun Route.utilApi() {
          call.respond("pong")
      }
     get("/isalive") {
-        application.log.info("isalive")
-        println("printisalive")
         call.respond(HttpStatusCode.OK)
     }
 
